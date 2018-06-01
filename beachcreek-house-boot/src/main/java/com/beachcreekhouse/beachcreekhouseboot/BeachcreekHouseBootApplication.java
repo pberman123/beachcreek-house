a0add3dea6931cf090fd582c38da58b998976f6d
Philip Berman <pberman@pivotal.io>
2018-06-01 13:52:02 -0500

    organized into boot dir

---
+++ beachcreek-house-boot/src/main/java/com/beachcreekhouse/beachcreekhouseboot/BeachcreekHouseBootApplication.java
@@ -0,0 +1,12 @@
+package com.beachcreekhouse.beachcreekhouseboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeachcreekHouseBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeachcreekHouseBootApplication.class, args);
+	}
+}
